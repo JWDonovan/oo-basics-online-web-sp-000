c27d9a4576b3ce590957ae9a203513bcc1b56e28
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 16:51:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,10 +4,6 @@ class Book
   attr_accessor :page_count
   attr_accessor :genre
 
-  def new(title)
-    :title = title
-  end
-
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
